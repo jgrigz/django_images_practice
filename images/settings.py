57515e4635b136a180a64eda827ca0942abd866d
Jamal Smilez <jgrigz1@gmail.com>
2022-05-31 14:38:09 -0400

    updated key

--- images/settings.py
+++ images/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)=fv1s&anit4w#($_+(#v2m0g$s%ob*%r^px4fib4-2zr5)06+'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
